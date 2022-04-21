32434bcdafbb1c520201181d5652f6b974145470
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2022-04-20 23:20:56 -0500

    Change to oak framework

--- routes/todos.routes.ts
+++ routes/todos.routes.ts
@@ -1,10 +1,7 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import { addTodo,getTotdos } from "../controllers/todos.controller.ts"; // Import controller methods
 
-const router = new Router();
-
-// Implement routes
-router.post("/api/todos", addTodo); // Add a todo
-router.get("/api/todos", getTotdos); // Add a todo
-
-export default router;
+export const router = new Router()
+  //Todo routes
+  .post("/api/todos", addTodo)
+  .get("/api/todos/", getTotdos);
--- server.ts
+++ server.ts
@@ -1,25 +1,10 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { router } from "./routes/todos.routes.ts";
 
 const app = new Application();
 
-app.use((context) => {
-  console.log("Hello World!");
-  context.response.body =
-        `<!DOCTYPE html>
-				 <html lang="en">
-				 <head>
-			   <meta charset="UTF-8">
-			   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-				 <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
-			   <title>Sanity <-> Deno</title>
-				 </head>
-				 <body>
-			   <div id="root">
-	       <h1> Deno and React with Sanity</h1>
-				 </div>
-				 </body>
-				 </html>`;
-});
+app.use(router.routes());
+
 
 
 await app.listen({ port: 4000 });
