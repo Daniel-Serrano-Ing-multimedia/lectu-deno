9b2c16eedbd488ba614f3481bd0039b078cc9bd0
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2022-04-20 23:27:00 -0500

    test envs

--- config/index.ts
+++ config/index.ts
@@ -1,9 +1,9 @@
-export const config:any {
-    DATA_API_KEY="zYIE7duhMObViVxmvD4lAFRLtANH9RUYSPlRSWBXBHIIqmJ0BwCNPJfQK0PG34CQ",
-    APP_ID="data-cjfni",
-    DB_PASSWORD="da6al9se3ri7",
-    DATA_SOURCE="Cluster0",
-    DATABASE="Lectu"
-} 
+export const config:any ={
+    DATA_API_KEY:"zYIE7duhMObViVxmvD4lAFRLtANH9RUYSPlRSWBXBHIIqmJ0BwCNPJfQK0PG34CQ",
+    APP_ID:"data-cjfni",
+    DB_PASSWORD:"da6al9se3ri7",
+    DATA_SOURCE:"Cluster0",
+    DATABASE:"Lectu"
+} ;
 //export {config} from "https://deno.land/x/dotenv/mod.ts";
 export const PORT = 4000;
